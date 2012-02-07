a088ee8752f245d331e55b0b93b915f839ee04fd
Ruben Torres <peladordegambas@hotmail.com>
2012-02-07 20:57:20 +0100

    Cambios en vista contactos

---
+++ application/views/contactos/index.php
@@ -0,0 +1,17 @@
+<table style="margin: auto" border="1">
+  <thead>
+    <th>Amigos</th>
+    <th>Operaciones</th>
+  </thead>
+  <tbody>
+    <?php foreach ($filas as $fila): ?>
+       <tr>
+	 <td> <?= $fila['nombre_amigo'] ?> </td>
+	   <?= form_open ('contactos/borrar') ?> </td>
+ 	 <td>
+	   <?= form_submit ('borrar', 'Borrar') ?> </td>
+	   <?= form_close() ?>
+      </tr>
+   <?php endforeach; ?>
+  </tbody>
+</table>
