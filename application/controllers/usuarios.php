622e5370bd0cfc254b3b34e36e498c6fbc7656f5
Sergio Rodriguez <srg301091@hotmail.com>
2012-02-07 19:35:31 +0100

    Carga de la vista login.php

--- application/controllers/usuarios.php
+++ application/controllers/usuarios.php
@@ -1,3 +1,17 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller {
+	
+	function index() {
+    	
+	}
+	
+	function login() {
     
-?>
+	}
+	
+	function logout() {
+	  
+	}
+
+}
