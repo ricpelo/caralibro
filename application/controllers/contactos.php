5bda9894bba5be36a94bc041551e5351db235a0b
Mariana Núñez Acosta <mariana_nuac89@hotmail.com>
2012-02-07 20:27:48 +0100

    Agregar archivos de contactos

---
+++ application/controllers/contactos.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Contactos extends CI_Controller {
+
+  function __construct() {
+    CI_Controller::__construct();
+    
+    //if (!$this->session->userdata('usuario')) {
+    //  $this->session->set_flashdata('mensaje', 'Se requiere autorización');
+    //  redirect('usuarios/login');
+    //} 
+  }
+
+  function index() {
+    $res = $this->db->query("select case when 1 = c.id_amigo1
+                                               then c.id_amigo2
+                                               else c.id_amigo1
+                                           end as id_amigo,
+                                          case when 1 = c.id_amigo1
+                                               then u2.nombre || ' ' || u2.apellidos
+                                               else u1.nombre || ' ' || u1.apellidos
+                                           end as nombre_amigo
+                                     from contactos c, usuarios u1, usuarios u2
+                                    where 1 in (id_amigo1, id_amigo2) and
+                                          c.id_amigo1 = u1.id and c.id_amigo2 = u2.id");
+    echo $res;
+//    $data['filas'] = $res->result_array();
+//    $config['base_url'] = 'http://localhost/web/codeigniter/empleados/index/';
+//    $config['total_rows'] = $res->num_rows();
+//    $config['per_page'] = 5;
+
+//    $this->pagination->initialize($config);
+ 
+//    if ($this->session->flashdata('mensaje')) {
+//      $data['mensaje'] = $this->session->flashdata('mensaje');
+//    } else {
+//      $data['mensaje'] = '';
+//    }
+//    $data['usuario'] = $this->session -> userdata('usuario');
+//    $this->load->view('empleados/index', $data);
+//    $this->pagination->create_links();
+	}
+
