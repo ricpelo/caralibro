31a299fe74f42a22e2c3c548d2cde5d8e76c5a9c
Jose Manuel Fernandez <josemanuel.tecnico@gmail.com>
2012-02-07 19:40:16 +0100

    Eliminar la necesidad del index.php

--- application/controllers/usuarios.php
+++ application/controllers/usuarios.php
@@ -1,3 +1,42 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller {
+  
+  function __construct() {
+    CI_Controller::__construct();
     
-?>
+    if (!$this->session->userdata('usuario')) {
+      $this->session->set_flashdata('mensaje', 'Se requiere autorización');
+      redirect('usuarios/login');
+    }
+  }
+  	
+  function index() {
+  	
+  }	
+
+  function login() {
+  	if ($this->input->post('login')) {
+      $email = $this->input->post('email');
+      $password = $this->input->post('password');
+      $res = $this->db->query("Select * from usuarios where email = ? and password = md5(?)", array($email, $password));
+      if ($res->num_rows() == 1) {
+        $this->session->set_userdata('usuario', $email);
+        redirect('usuarios/index');
+      } else {
+        $mensaje = 'Error: usuario o contraseña incorrectos';
+      }
+
+    } else {
+      $mensaje = $this->session->flashdata('mensaje');
+    }
+
+    $this->load->view('usuarios/login', array('mensaje' => $mensaje));
+  }
+  
+  function logout() {
+    $this->session->unset_userdata('usuario');
+    redirect('usuarios/login');
+  }
+
+}
