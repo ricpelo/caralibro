dca670deae0c0c23637b7830a477363ce2f78110
Mariana Núñez Acosta <mariana_nuac89@hotmail.com>
2012-02-08 20:21:37 +0100

    arreglo lista mis amigos

--- application/models/contacto.php
+++ application/models/contacto.php
@@ -2,17 +2,22 @@
 
 class Contacto extends CI_Model {
   
-  function obtener_mis_amigos() {
-    return $this->db->query("select case when 1 = c.id_amigo1
+  function obtener_mis_amigos($id) {
+    return $this->db->query("select case when $id = c.id_amigo1
                                                then c.id_amigo2
                                                else c.id_amigo1
                                            end as id_amigo,
-                                          case when 1 = c.id_amigo1
+                                          case when $id = c.id_amigo1
                                                then u2.nombre || ' ' || u2.apellidos
                                                else u1.nombre || ' ' || u1.apellidos
                                            end as nombre_amigo
                                from contactos c, usuarios u1, usuarios u2
-                              where 1 in (id_amigo1, id_amigo2) and
+                              where $id in (id_amigo1, id_amigo2) and
                     c.id_amigo1 = u1.id and c.id_amigo2 = u2.id")->result_array();
   }
-}
+
+  function obtener_id() {
+    $email = $this->session->userdata('usuario');
+    return $this->db->query("select id from usuarios where email = '$email'")->row_array();
+  }
+} 
