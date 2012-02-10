4804b178fbfb326ac3cbc2d915895aea93dfc209
Antonio Gallego <tonisupersonico@gmail.com>
2012-02-10 18:44:51 +0100

    Actualizado el login

--- application/views/template.php
+++ application/views/template.php
@@ -5,26 +5,27 @@
   </head>
 <body>
 
-		<div id="header">
+	<div id="header">
+		<p>Caralibro</p>
+	</div>
 
-			<p>Caralibro</p>
+	<?php if (isset($usuario)): ?>
+  <div id="menubotones">
+		<table border="0">
+			<tbody>
+				<tr>
+					<td id="usuario"><?= $usuario ?></td>
+				</tr>
+			</tbody>
+		</table>
+  </div>
+	<?php endif; ?>
 
-		</div>
-    <div id="menubotones">
-    <table border="0">
-    <tbody>
-    <tr>
-    <td id="usuario"><?= $usuario ?></td>
-    </tr>
-    </tbody>
-    </table>
-    </div>
+  <div id="contents"><?= $contents ?></div>
 
-    <div id="contents"><?= $contents ?></div>
-
-    <div id="footer">
-			<p>Copyright 2012 IES Doñana</p>
-		</div>
+  <div id="footer">
+		<p>Copyright 2012 IES Doñana</p>
+	</div>
 
 </body>
 </html>
