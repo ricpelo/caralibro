7f07a2d923526de751c75a75e2fbee5a10d6c52c
Antonio Gallego <tonisupersonico@gmail.com>
2012-02-17 18:50:14 +0100

    Cambios en la búsqueda de contactos

--- application/views/contactos/buscar.php
+++ application/views/contactos/buscar.php
@@ -3,19 +3,19 @@
 </div>
 
 <table>
-  <thead>
-    <th> Nombre </th>
-    <th> </th>
-  </thead>
   <tbody>
-    <?php foreach ($filas as $fila): ?>
-      <tr>
-             
-        <td> <?= anchor("/muros/index/{$fila['id']}", $fila['nombre']) ?> </td>
-	           <?= form_open ('contactos/agregar_amigo') ?> </td>
- 	      <td> <?= form_hidden('id_solicitado', $fila['id']) ?>
-	           <?= form_submit ('agregar', 'Agregar Amigo') ?> </td>
-	           <?= form_close() ?>
+    <?php foreach ($filas as $fila): ?>			
+      <tr>   
+				<?= form_open ('contactos/agregar_amigo') ?>					   
+		      <td> 
+						<?= anchor("/muros/index/{$fila['id']}", $fila['nombre']) ?>
+					</td>
+					<td>&nbsp -- &nbsp</td>
+	 	      <td> 
+						<?= form_hidden('id_solicitado', $fila['id']) ?>
+			      <?= form_submit ('agregar', 'Agregar contacto') ?> 
+					</td>
+	      <?= form_close() ?>
       </tr>
     <?php endforeach; ?>
   </tbody>
