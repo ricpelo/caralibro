edc58bdf63fc5ea445f40390c2fba66d6b3873d6
Mariana Núñez Acosta <mariana_nuac89@hotmail.com>
2012-02-20 16:24:45 +0100

    Arreglo sql

--- application/views/contactos/index.php
+++ application/views/contactos/index.php
@@ -4,23 +4,24 @@
   </div>
 <?php endif; ?>
 
-<table style="margin: auto">
-  <thead>
-    <th>Amigos</th>
-    <th></th>
-  </thead>
-  <tbody>
-    <?php foreach ($filas as $fila): ?>
-       <tr>
-	      <td> <?= anchor("/muros/index/{$fila['id_amigo']}", $fila['nombre_amigo']) ?> </td>
-	           <?= form_open ('contactos/borrar_amigo') ?> </td>
- 	      <td>
-             <?= form_hidden('id_amigo', $fila['id_amigo']) ?>
-	           <?= form_submit ('borrar', '         Borrar       ') ?> </td>
-	           <?= form_close() ?>
-       </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <table style="margin: auto">
+    <thead>
+      <th>Amigos</th>
+      <th></th>
+    </thead>
+    <tbody>
+      <?php foreach ($filas as $fila): ?>
+         <tr>
+	        <td> <?= anchor("/muros/index/{$fila['id_amigo']}", $fila['nombre_amigo']) ?> 
+          </td>
+	             <?= form_open ('contactos/borrar_amigo') ?> </td>
+   	      <td>
+               <?= form_hidden('id_amigo', $fila['id_amigo']) ?>
+	             <?= form_submit ('borrar', '         Borrar       ') ?> </td>
+	             <?= form_close() ?>
+         </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 
 <p><?= anchor('/contactos/buscar', 'Buscar nuevos amigos') ?></p>
